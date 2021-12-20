62ded54e177a9230ddfd1327097da8f4c6d6a598
Marc <marcjacques@me.com>
2021-12-20 14:11:38 -0500

    Create 3 views to login to account

--- Programmatic Login/ViewController.swift
+++ Programmatic Login/ViewController.swift
@@ -8,6 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let loginContentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let usernameTextField: UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = .white
+        textField.placeholder = "Username"
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.isSecureTextEntry = true
+        textField.backgroundColor = .white
+        textField.placeholder = "Password"
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .blue
+        button.setTitle("Login", for: .normal)
+        button.tintColor = .white
+        button.layer.cornerRadius = 5
+        button.clipsToBounds = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
