cd27fa922963f18f1fa0759215975595f6f4d355
Marc <marcjacques@me.com>
2021-12-20 14:12:53 -0500

    Present constrained views

--- Programmatic Login/ViewController.swift
+++ Programmatic Login/ViewController.swift
@@ -48,7 +48,36 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemRed
+        
+        loginContentView.addSubview(usernameTextField)
+        loginContentView.addSubview(passwordTextField)
+        loginContentView.addSubview(loginButton)
+        
+        view.addSubview(loginContentView)
+        setUpAutoLayout()
+    }
+    
+    func setUpAutoLayout() {
+        loginContentView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        loginContentView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        loginContentView.heightAnchor.constraint(equalToConstant: view.frame.height/3).isActive = true
+        loginContentView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        usernameTextField.topAnchor.constraint(equalTo: loginContentView.topAnchor, constant: 40).isActive = true
+        usernameTextField.leftAnchor.constraint(equalTo: loginContentView.leftAnchor, constant: 20).isActive = true
+        usernameTextField.rightAnchor.constraint(equalTo: loginContentView.rightAnchor, constant: -20).isActive = true
+        usernameTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        passwordTextField.topAnchor.constraint(equalTo: usernameTextField.bottomAnchor, constant: 20).isActive = true
+        passwordTextField.leftAnchor.constraint(equalTo: loginContentView.leftAnchor, constant: 20).isActive = true
+        passwordTextField.rightAnchor.constraint(equalTo: loginContentView.rightAnchor, constant: -20).isActive = true
+        passwordTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        loginButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 20).isActive = true
+        loginButton.leftAnchor.constraint(equalTo: loginContentView.leftAnchor, constant: 180).isActive = true
+        loginButton.rightAnchor.constraint(equalTo: loginContentView.rightAnchor, constant: -180).isActive = true
+        loginButton.heightAnchor.constraint(equalToConstant: 25).isActive = true
     }
 
 
